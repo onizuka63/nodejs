6185b81fd9f7f76a15399b0d18d55f08fd391c38
onizuka63 <manucorbeau@gmail.com>
2015-03-14 22:23:53 +0100

    return pathname (string) if the parameter is a string given by routes (array string), 404 (int) if unknown

--- router.js
+++ router.js
@@ -1,5 +1,24 @@
+// The router analyzes the request path and return the appropriate response. The response will be instantiated by the server and given back to the user.
+
+var routes = [
+    '/'       ,
+    '/blog'   ,
+    '/about'  ,
+    '/contact',
+];
+
 function route(pathname) {
     console.log("About to route a request for " + pathname);
+
+    var route_id = routes.indexOf(pathname)
+    if (route_id == -1) {
+        console.log("404 - not found");
+        return 404;
+    }
+    else {
+        console.log(pathname);
+        return pathname;
+    }
 }
 
 exports.route = route;
